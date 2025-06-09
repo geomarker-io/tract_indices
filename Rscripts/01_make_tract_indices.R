d0aeb2c8b1ad935567e9a35cbd14a31f20630f74
erikarasnick <erika.rasnick@gmail.com>
2025-06-09 14:09:53 -0400

    make tract indices; waiting for dep_index

---
+++ Rscripts/01_make_tract_indices.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(dpkg)
+
+# read in and join all data
+fls <- list.files(path = 'data', pattern = '*.rds')
+d <- map(glue::glue('data/{fls}'), readRDS)
+
+d <- purrr::reduce(d, .f = left_join, by = 'census_tract_id_2020')
+
+# round all variables to 3 signif digits
+d <- d |>
+  mutate(across(where(is.numeric), \(x) signif(x, 3)))
+
+# create dataset attrs
+d_dpkg <-
+  d |>
+  dpkg::as_dpkg(
+    name = "tract_indices",
+    title = "Census Tract-Level Neighborhood Indices",
+    version = "1.0.0",
+    homepage = "https://geomarker.io/tract_indices",
+    description = paste(readLines(fs::path("metadata.md")), collapse = "\n")
+  )
+
+# write tdr
+dpkg::dpkg_gh_release(d_dpkg, draft = FALSE)
