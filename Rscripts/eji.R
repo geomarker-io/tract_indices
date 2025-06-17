e11583696f684ffab96ee164b3f3a0e551e8bb6b
erikarasnick <erika.rasnick@gmail.com>
2025-06-17 12:29:16 -0400

    remove eji rank vars

--- Rscripts/eji.R
+++ Rscripts/eji.R
@@ -6,21 +6,13 @@ d <- read_csv("data-raw/EJI_2024_United_States.csv") |>
   select(
     census_tract_id_2020 = GEOID_2020,
     high_ozone_days = E_OZONE,
-    high_ozone_rank = EPL_OZONE,
     high_pm_days = E_PM,
-    high_pm_rank = EPL_PM,
     diesel_pm_conc = E_DSLPM,
-    diesel_pm_rank = EPL_DSLPM,
     air_toxics_cancer_risk = E_TOTCR,
-    air_toxics_cancer_risk_rank = EPL_TOTCR,
     prop_near_parks = E_PARK,
-    lack_of_parks = EPL_PARK,
     walkability = E_WLKIND,
-    lack_of_walkability = EPL_WLKIND,
     prop_near_railway = E_RAIL,
-    railway_proximity_rank = EPL_RAIL,
     prop_near_airport = E_AIRPRT,
-    airport_proximity_rank = EPL_AIRPRT
   )
 
 saveRDS(d, 'data/eji.rds')
